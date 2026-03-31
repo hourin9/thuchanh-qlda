f0e5acd3728a0626b4dbf5f10708f9613389cd4b
Hoang Han <nguyenhoangnhan20052005@gmail.com>
2026-03-31 08:26:01 +0700

    con cac

--- vector.hh
+++ vector.hh
@@ -25,10 +25,7 @@ public:
         float &operator[](size_t idx);
         float operator[](size_t idx) const;
 
-        // Thanh vien 2 luu y: ham nay thuc hien deep copy. Thay vi
-        // chi gan _data cua FloatVector nay vao _data cua FloatVector
-        // kia thi phai copy tung phan tu cua FloatVector kia, de tranh
-        // bug double free.
+        
         FloatVector &operator=(const FloatVector); // Gan vector nay
                                                    // vao vector kia.
 
@@ -65,5 +62,7 @@ private:
 
         float *_data;
         size_t _size, _capacity;
+        float _default_capacity;
+        float _capacity_multiplier;
 };
 
